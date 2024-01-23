a40bea721d0a847a627af5882e528e4789b2547b
Vikash <empirevikash@gmail.com>
2024-01-23 23:41:37 +0530

    workingOnClickNavigation

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BiHomeAlt, BiUser } from "react-icons/bi";
-import { BsClipboardData, BsBriefcase, BsChatSquare } from "react-icons/bs";
+import { BsClipboardData, BsBriefcase, BsChatSquareText } from "react-icons/bs";
 
 // Link
 import { Link } from "react-scroll";
@@ -11,17 +11,20 @@ const Nav = () => {
       <div className="container mx-auto">
         {/* nav inner */}
         <div className="w-full bg-black/20 h-[96px] backdrop-blur-2xl rounded-full max-w-[460px] mx-auto px-5 flex justify-between text-2xl text-white/50 items-center">
-          <Link className=" cursor-pointer w-[60px] h-[60px] flex items-center">
+          <Link to="home" activeClass="active" smooth={true} spy={true} offset={-200} className=" cursor-pointer w-[60px] h-[60px] flex items-center">
             <BiHomeAlt />
           </Link>
-          <Link className=" cursor-pointer w-[60px] h-[60px] flex items-center">
+          <Link to="about" activeClass="active" smooth={true} spy={true} className=" cursor-pointer w-[60px] h-[60px] flex items-center">
             <BiUser />
           </Link>
-          <Link className=" cursor-pointer w-[60px] h-[60px] flex items-center">
+          <Link to="services" activeClass="active" smooth={true} spy={true} className=" cursor-pointer w-[60px] h-[60px] flex items-center">
             <BsClipboardData />
           </Link>
-          <Link className=" cursor-pointer w-[60px] h-[60px] flex items-center">
+          <Link to="work" activeClass="active" smooth={true} spy={true} className=" cursor-pointer w-[60px] h-[60px] flex items-center">
             <BsBriefcase />
+          </Link
+          ><Link to="contact" activeClass="active" smooth={true} spy={true} className=" cursor-pointer w-[60px] h-[60px] flex items-center">
+            <BsChatSquareText />
           </Link>
         </div>
       </div>
