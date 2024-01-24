8ffabcbb33422ee96b6b4ef7d4540def74a1946d
Vikash <empirevikash@gmail.com>
2024-01-24 22:16:28 +0530

    workingOnBanner

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Logo from '../assets/logo.svg'
 
 const Header = () => {
-  return <header className="p-8" >
+  return <header className="p-4" >
     <div className="container mx-auto">
       <div className='flex justify-between items-center'>
         {/* logo */}
