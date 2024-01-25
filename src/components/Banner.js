c885d25b9ba8b2324e8c6a17f114a2ac88b0ea7d
Vikash <empirevikash@gmail.com>
2024-01-25 22:33:17 +0530

    Banner done

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,45 +1,110 @@
-import React from 'react';
-import Image from "../assets/avatar.svg"
-import {FaGithub,FaYoutube,FaDribbble} from "react-icons/fa"
-import {TypeAnimation} from "react-type-animation"
-import {motion} from "framer-motion"
-import {fadeIn} from "../variants"
+import React from "react";
+import Image from "../assets/avatar.svg";
+import { FaGithub, FaDribbble, FaLinkedin } from "react-icons/fa";
+import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
 
 const Banner = () => {
-  return <section className='section' id='home'>
-    <div className="container mx-auto">
-      <div>
-        {/* text */}
-        <div>
-          <h1>VIKASH <span>SHARMA</span></h1>
-          <div className='mb-6 text-[36px] font-secondary font-semibold uppercase leading-[1] lg:text-[60px]'>
-            <span className=' text-white mr-4'>I am a </span>
-            <TypeAnimation sequence={[
-              'Frontend Devloper',
-              2000,
-              'Designer',
-              2000,
-              'Frontend Devloper',
-              2000,
-              'Designer',
-              2000,
-            ]}
-            speed={50}
-            className='text-accent'
-            wrapper='span'
-            repeat={Infinity}
-            />
-           
+  return (
+    <section
+      className="min-h-[85vh] lg:min-h-[78vh] flex items-center before:"
+      id="home"
+    >
+      <div className="container mx-auto">
+        <div className=" flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12">
+          {/* text */}
+          <div className="flex-1 text-center font-secondary lg:text-left">
+            <motion.h1
+              variants={fadeIn("up", 0.3)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="text-[55px] font-bold leading-[0.8] lg:text-[70px]"
+            >
+              VIKASH <span>SHARMA</span>
+            </motion.h1>
+            <motion.div
+              variants={fadeIn("up", 0.4)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="mb-6 text-[36px] font-secondary font-semibold uppercase leading-[1] lg:text-[40px]"
+            >
+              <span className=" text-white mr-4">I am a</span>
+              <TypeAnimation
+                sequence={[
+                  "Frontend Devloper",
+                  2000,
+                  "Designer",
+                  2000,
+                  "Frontend Devloper",
+                  2000,
+                  "Designer",
+                  2000,
+                ]}
+                speed={50}
+                className="text-accent"
+                wrapper="span"
+                repeat={Infinity}
+              />
+            </motion.div>
+            <motion.p
+              variants={fadeIn("up", 0.5)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="mb-8 max-w-lg mx-auto lg:mx-0"
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+              pariatur qui quam. Facere perspiciatis laboriosam deserunt ad
+              mollitia, numquam repellendus.z
+            </motion.p>
+            <motion.div
+              variants={fadeIn("up", 0.6)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="flex max-w-max gap-x-6 items-center mx-auto lg:mx-0"
+            >
+              <button className="btn btn-lg">Contact me</button>
+              <a href="#" className="text-gradient btn-link">
+                My Portfolio
+              </a>
+            </motion.div>
+            {/* social */}
+            <motion.div
+              variants={fadeIn("up", 0.8)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="flex text-[20px] gap-x-6 mt-4 max-w-max mx-auto lg:mx-6 "
+            >
+              <a href="#">
+                <FaLinkedin />
+              </a>
+              <a href="#">
+                <FaGithub />
+              </a>
+              <a href="#">
+                <FaDribbble />
+              </a>
+            </motion.div>
           </div>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora pariatur qui quam. Facere perspiciatis laboriosam deserunt ad mollitia, numquam repellendus.z</p>
-        </div>
-        {/* Imgae */}
-        <div>
-          <img src={Image} alt="" />
+          {/* Imgae */}
+          <motion.div
+            variants={fadeIn("down", 0.8)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.7 }}
+            className="hidden lg:flex flex-1 max-w-[320px] lg:max-w-[400px] mx-auto"
+          >
+            <img src={Image} alt="" className="" />
+          </motion.div>
         </div>
       </div>
-    </div>
-  </section>;
+    </section>
+  );
 };
 
 export default Banner;
