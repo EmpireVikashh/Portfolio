0e7f99c3fc35a3001ea3679137934d3dbffe007d
Vikash <empirevikash@gmail.com>
2024-01-25 22:33:33 +0530

    workingOneAbout

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,74 @@
-import React from 'react';
+import React from "react";
+//countup
+import CountUp from "react-countup";
+import { useInView } from "react-intersection-observer";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
 
 const About = () => {
-  return <div className='section' id='about'>About</div>;
+  const [ref, inView] = useInView({
+    threshold: 0.5,
+  });
+  return (
+    <section className="section  mt-10" id="about" ref={ref}>
+      <div className="container mxauto">
+        <div className="flex items-center">
+          {/* image */}
+          <div className="flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top">
+            
+          </div>
+          {/* text */}
+          <div className="flex-1">
+            <h2 className="h2 text-accent">About me.</h2>
+            <h3 className="h3 mb-1">
+              I'm a passionate Frontend Devloper, I would be very thankful to
+              work with you.
+            </h3>
+            <p className="mb-4">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Quibusdam eveniet, modi veritatis id eos distinctio error nihil
+              tenetur quae totam
+            </p>
+            <div className="flex gap-x-6 lg:gap-x-10 mb-8">
+              <div>
+                <div className="text-[40px] font-tertiary text-gradient mb-2">
+                {inView ? <CountUp start={0} end={1} duration={5} /> : <>1</> }.4
+                </div>
+                <div className="font-primary text-sm tracking-[2px]">
+                  Years of<br />
+                  Learning Experience
+                </div>
+              </div>
+              <div>
+                <div className="text-[40px] font-tertiary text-gradient mb-2">
+                  {inView ?<CountUp start={0} end={5} duration={5} /> :<>5</>}+
+                </div>
+                <div className="font-primary text-sm tracking-[2px]">
+                  Projects<br />
+                  Completed
+                </div>
+              </div>
+              <div>
+                <div className="text-[40px] font-tertiary text-gradient mb-2">
+                 { inView?<CountUp start={0} end={4} duration={5} />:<>4</>}.5 
+                </div>
+                <div className="font-primary text-sm tracking-[2px]">
+                  Star of<br />
+                  my Perseverance
+                </div>
+              </div>
+            </div>
+            <div className="flex gap-x-8 items-center">
+              <button className="btn btn-lg" >Contact me</button>
+              <a href="#" className="text-gradient btn-link ">
+                My Portfolio
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
