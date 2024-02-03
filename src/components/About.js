f31f215eaf843f7c674be2833ce58f269be8e962
Vikash <empirevikash@gmail.com>
2024-02-03 23:08:23 +0530

    about done

--- src/components/About.js
+++ src/components/About.js
@@ -12,13 +12,23 @@ const About = () => {
   return (
     <section className="section  mt-10" id="about" ref={ref}>
       <div className="container mxauto">
-        <div className="flex items-center">
+        <div className="flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen">
           {/* image */}
-          <div className="flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top">
-            
-          </div>
+          <motion.div
+            variants={fadeIn("right", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top"
+          ></motion.div>
           {/* text */}
-          <div className="flex-1">
+          <motion.div
+            variants={fadeIn("left", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1"
+          >
             <h2 className="h2 text-accent">About me.</h2>
             <h3 className="h3 mb-1">
               I'm a passionate Frontend Devloper, I would be very thankful to
@@ -32,39 +42,45 @@ const About = () => {
             <div className="flex gap-x-6 lg:gap-x-10 mb-8">
               <div>
                 <div className="text-[40px] font-tertiary text-gradient mb-2">
-                {inView ? <CountUp start={0} end={1} duration={5} /> : <>1</> }.4
+                  {inView ? <CountUp start={0} end={1} duration={5} /> : <>1</>}
+                  .4
                 </div>
                 <div className="font-primary text-sm tracking-[2px]">
-                  Years of<br />
+                  Years of
+                  <br />
                   Learning Experience
                 </div>
               </div>
               <div>
                 <div className="text-[40px] font-tertiary text-gradient mb-2">
-                  {inView ?<CountUp start={0} end={5} duration={5} /> :<>5</>}+
+                  {inView ? <CountUp start={0} end={5} duration={5} /> : <>5</>}
+                  +
                 </div>
                 <div className="font-primary text-sm tracking-[2px]">
-                  Projects<br />
+                  Projects
+                  <br />
                   Completed
                 </div>
               </div>
               <div>
                 <div className="text-[40px] font-tertiary text-gradient mb-2">
-                 { inView?<CountUp start={0} end={4} duration={5} />:<>4</>}.5 
+                  {inView ? <CountUp start={0} end={4} duration={5} /> : <>4</>}
+                  .5
                 </div>
                 <div className="font-primary text-sm tracking-[2px]">
-                  Star of<br />
+                  Star of
+                  <br />
                   my Perseverance
                 </div>
               </div>
             </div>
             <div className="flex gap-x-8 items-center">
-              <button className="btn btn-lg" >Contact me</button>
+              <button className="btn btn-lg">Contact me</button>
               <a href="#" className="text-gradient btn-link ">
                 My Portfolio
               </a>
             </div>
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
