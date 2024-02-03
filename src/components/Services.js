56783b1209fd9d179807755e39b7f251a7dfcbd0
Vikash <empirevikash@gmail.com>
2024-02-03 23:10:51 +0530

    working in services

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import {BsArrowUpRight} from "react-icons/bs"
+import {motion} from "framer-motion"
+import { fadeIn } from '../variants';
+
+
+
 
 const Services = () => {
-  return <div className='section' id='services'>Services</div>;
+  return <div className='section' id='services'>
+    
+  </div>;
 };
 
 export default Services;
